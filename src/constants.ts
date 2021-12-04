4c78c18a54c00aa9ba6021537a31ec77fe8a9b0a
The GOTH Token Network <jamiewhite121988@gmail.com>
2021-12-04 15:40:08 +0000

    Update Factory Address & Init Hash Code

--- src/constants.ts
+++ src/constants.ts
@@ -20,11 +20,11 @@ export enum Rounding {
 }
 
 export const FACTORY_ADDRESS: { [chainId in ChainId]: string } = {
-  [ChainId.FUJI]: '0xE4A575550C2b460d2307b82dCd7aFe84AD1484dd',
-  [ChainId.AVALANCHE]: '0xefa94DE7a4656D787667C749f7E1223D71E9FD88'
+  [ChainId.FUJI]: '0xbb595F82e58107EeE1696bF4436A55B720c3fAF8',
+  [ChainId.AVALANCHE]: '0xbb595F82e58107EeE1696bF4436A55B720c3fAF8'
 }
 
-export const INIT_CODE_HASH = '0x40231f6b438bce0797c9ada29b718a87ea0a5cea3fe9a771abdd76bd41a3e545'
+export const INIT_CODE_HASH = 'eecad807190911afceb8b86694b83279dc50e5d6b8ab73a1274e4e6e6d5b86c1'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
